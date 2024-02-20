0c442e4911695fb9efdeceac3ab9bfc176f472ec
Exotik850 <kidkool850@gmail.com>
2024-02-20 13:05:52 -0600

    make fullscreen hook work on web

--- src/hooks/mod.rs
+++ src/hooks/mod.rs
@@ -3,11 +3,27 @@ mod use_slider;
 mod use_tween;
 #[cfg(feature="clipboard")]
 mod use_clipboard;
-#[cfg(feature="desktop")]
+
 mod use_fullscreen;
+pub use use_fullscreen::*;
 // #[cfg(feature="controller")]
 // mod use_controller;
-mod use_toast;
+
+/// This module contains the implementation of a toast notification system.
+/// 
+/// The `ToastType` enum represents the different types of toast notifications that can be displayed.
+/// 
+/// The `ToastPosition` enum represents the different positions where toast notifications can be displayed.
+/// 
+/// The `ToastInfo` struct represents the information of a toast notification, including its title, description, height, and options.
+/// 
+/// The `ToastOptions` struct represents the options for a toast notification, including its type, icon, duration, action, and position.
+/// 
+/// The `ToastManager` struct represents the manager for toast notifications, including the list of toasts and their heights.
+/// 
+/// The `toast` function is used to display a toast notification with the specified title, description, and options.
+pub mod use_toast;
+
 
 #[cfg(feature="desktop")] 
 pub use use_fullscreen::*;
--- src/hooks/use_fullscreen.rs
+++ src/hooks/use_fullscreen.rs
@@ -1,68 +1,127 @@
-use dioxus::{desktop::{use_window, use_wry_event_handler, DesktopContext}, prelude::*};
+use dioxus::prelude::*;
 
 #[cfg(feature="desktop")]
+use dioxus::desktop::{use_window, use_wry_event_handler, DesktopContext};
+#[cfg(feature="web")]
+use web_sys::wasm_bindgen::{closure::Closure, JsCast};
+
 #[derive(Clone)]
 pub struct FullScreenManager {
   fullscreen: Signal<bool>,
   toggle: Signal<bool>,
-  window: DesktopContext
+  #[cfg(feature = "desktop")]
+  window: DesktopContext,
+  #[cfg(feature = "web")]
+  document: Signal<(web_sys::Document, web_sys::HtmlElement)>,
 }
 
-#[cfg(feature="desktop")]
 impl FullScreenManager {
+  #[cfg(feature = "desktop")]
   pub fn set(&mut self, val: bool) {
     self.fullscreen.set(val);
     self.window.set_maximized(val);
+  }
+
+  #[cfg(feature = "web")]
+  pub fn set(&mut self, val: bool) {
+    let doc = self.document.read();
+
+    let open = if val {
+      let result = doc.1.request_fullscreen();
+      result.is_ok()
+    } else {
+      doc.0.exit_fullscreen();
+      false
+    };
+    self.fullscreen.set(open);
   }
 
   pub fn toggle(&mut self) {
     self.toggle.set(true);
   }
 
-  pub fn value(&self) -> bool {
+  pub fn is_fullscreen(&self) -> bool {
     *self.fullscreen.read()
   }
 }
 
-#[cfg(feature="desktop")]
 pub fn use_fullscreen() -> FullScreenManager {
-  let mut fullscreen = use_signal(|| false);
-  let mut toggle = use_signal(|| false);
-  let window = use_window();
+    let mut fullscreen = use_signal(|| false);
+    let mut toggle = use_signal(|| false);
 
-  let window_c = window.clone();
-
-  use_wry_event_handler(move |evt, window| {
-    match evt {
-        dioxus::desktop::tao::event::Event::WindowEvent { window_id, event, .. } if *window_id == window_c.id() => {
-          match event {
-            dioxus::desktop::WindowEvent::Resized(_) if window_c.is_maximized() => {
-              fullscreen.set(true);
-            },
-            dioxus::desktop::WindowEvent::Resized(_) => {
-              fullscreen.set(false);
+    #[cfg(feature = "desktop")]
+    {
+        let window = use_window();
+        let window_c = window.clone();
+        use_wry_event_handler(move |evt, window| match evt {
+            dioxus::desktop::tao::event::Event::WindowEvent {
+                window_id, event, ..
+            } if *window_id == window_c.id() => match event {
+                dioxus::desktop::WindowEvent::Resized(_) if window_c.is_maximized() => {
+                    fullscreen.set(true);
+                }
+                dioxus::desktop::WindowEvent::Resized(_) => {
+                    fullscreen.set(false);
+                }
+                _ => (),
             },
             _ => (),
+        });
+        let window_c = window.clone();
+        use_effect(move || {
+            if toggle() {
+                let not_fullscreen = !fullscreen();
+                window_c.set_maximized(not_fullscreen);
+                fullscreen.set(not_fullscreen);
+                toggle.set(false);
+            }
+        });
+
+        FullScreenManager {
+            fullscreen,
+            toggle,
+            window,
         }
-        },
-        _ => (),
     }
-  });
 
-  let window_c = window.clone();
+    #[cfg(feature="web")]
+    {
+      let document = use_signal(|| {
+        let document = web_sys::window().expect("Need a window to get document from").document().expect("Need a document");
+        let element = document.body().expect("Need a document body to request fullscreen on");
+        (document, element)
+      });
 
-  use_effect(move || if toggle() {
-    if fullscreen() {
-      window_c.set_maximized(false);
-      fullscreen.set(false);
-    } else {
-      window_c.set_maximized(true);
-      fullscreen.set(true);
+      let closure = Closure::new(move || {
+        fullscreen.set(document.read().0.fullscreen());
+      });
+      document.read().0.set_onfullscreenchange(Some(closure.as_ref().unchecked_ref()));
+
+      use_effect(move || {
+        if toggle() {
+          let full = if fullscreen() {
+            document.read().0.exit_fullscreen();
+            false
+          } else {
+            document.read().1.request_fullscreen().is_ok()
+          };
+          fullscreen.set(full);
+          toggle.set(false);
+        }
+      });
+
+      FullScreenManager {
+          fullscreen,
+          toggle,
+          document,
+      }
     }
-    toggle.set(false);
-  });
 
-  FullScreenManager {
-    fullscreen, toggle, window
-  }
+    #[cfg(not(any(feature = "desktop", feature = "web")))]
+    {
+      FullScreenManager {
+          fullscreen,
+          toggle,
+      }
+    }
 }
