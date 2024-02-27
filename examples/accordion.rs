c654566ceb13bc62ad2a80247eb79fe264853b7f
kidkool850@gmail.com <kidkool850@gmail.com>
2024-02-27 09:13:45 -0600

    popover prototype

--- examples/accordion.rs
+++ examples/accordion.rs
@@ -6,19 +6,24 @@ use texo_ui::elements::*;
 
 pub fn main() {
     dioxus_logger::init(log::LevelFilter::Info).unwrap();
+    // wasm_logger::init(wasm_logger::Config::default());
     launch(App)
 }
 
 #[component]
 fn App() -> Element {
-    let mut fullscreen = use_fullscreen();
-    let is_fullscreen = fullscreen.is_fullscreen();
+    // let mut fullscreen = use_fullscreen();
+    // let is_fullscreen = fullscreen.is_fullscreen();
     let mut open = use_signal(|| false);
 
-    let actions = Signal::new(vec![
-      CommandAction::new(|_| toast("HELP", None, Default::default()), "Hello".to_string(), "Say hello".to_string(), None, None),
-      CommandAction::new(|_| println!("World"), "World".to_string(), "Say world".to_string(), None, None),
-    ]);
+    // let actions = use_signal(|| vec![
+    //   CommandAction::new(|_| toast("HELP", None, Default::default()), "Hello".to_string(), "Say hello".to_string(), None, None),
+    //   CommandAction::new(|_| log::info!("World"), "World".to_string(), "Say world".to_string(), None, Some("world".to_string())),
+    //   CommandAction::new(|_| log::info!("eager"), "weigh".to_string(), "Say explore".to_string(), None, None),
+    //   CommandAction::new(|_| log::info!("comfortable"), "atmosphere".to_string(), "Say forty".to_string(), None, None),
+    //   CommandAction::new(|_| log::info!("his"), "these".to_string(), "Say syllable".to_string(), None, None),
+    //   CommandAction::new(|_| log::info!("chief"), "rich".to_string(), "Say forward".to_string(), None, None),
+    // ]);
 
     rsx!(
         link {
@@ -27,29 +32,34 @@ fn App() -> Element {
         }
 
         Toaster {}
-
+ 
         Button {
-          onclick: move |_| fullscreen.toggle(),
-          "Click me!"
-        }
-
-        Button {
-          onclick: move |_| open.toggle(),
+          onclick: move |_| {
+            open.toggle();
+            log::info!("Open Command Palette!");
+          }, 
           "Open Command Palette!"
         }
 
-        span {
-          if is_fullscreen {
-            "I'm fullscreen!"
-          } else {
-            "I'm not fullscreen!"
+        Popover {
+          open, 
+          content: rsx!(
+            "Hello, World!"
+          ),
+
+          button {
+            onclick: move |_| {
+              open.toggle();
+              log::info!("Open Command Palette!");
+            },
+            "Open Command Palette!"
           }
         }
 
-        CommandPalette {
-          actions,
-          placeholder: "Type a command...",
-          visible: open
-        }
+        // CommandPalette {
+        //   actions,
+        //   placeholder: "Type a command...",
+        //   visible: open
+        // }
     )
 }
